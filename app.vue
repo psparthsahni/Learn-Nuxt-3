1c17873bbc5c37658266038d7e9a25b47c6ef4d4
Parth Sahni <parth@faimtech.in>
2022-08-19 22:44:46 +0530

    Nuxt 3 basics

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>
+      <input type="text" v-model="greetings">
+    </p>
+    <p>
+      {{ greetings }} ({{ greetings.length }})
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      greetings: 'Nuxt 3'
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.greetings = 'Nuxt 3 Absolute Basics'
+    }, 3000)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
