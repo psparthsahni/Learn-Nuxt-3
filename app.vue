f4562def2cb61bb11728ea0fea881df635ca08a2
Parth Sahni <parth@faimtech.in>
2022-08-20 03:16:31 +0530

    component props

--- app.vue
+++ app.vue
@@ -1,69 +1,23 @@
 <template>
   <div>
-    <section v-show="inProgressAssignments.length">
-      <h2>Todo Assignments</h2>
-      <ul>
-        <li v-for="assignment in inProgressAssignments" :key="assignment.id">
-          <label>
-            <input type="checkbox" v-model="assignment.complete"> {{ assignment.name }}
-          </label>
-        </li>
-      </ul>
-    </section>
-    <section v-show="completedAssignments.length">
-      <h2>Completed Assignments</h2>
-      <ul>
-        <li v-for="assignment in completedAssignments" :key="assignment.id">
-          <label>
-            <input type="checkbox" v-model="assignment.complete"> {{ assignment.name }}
-          </label>
-        </li>
-      </ul>
-    </section>
+    <ButtonComponent type="primary">
+      Hello World
+    </ButtonComponent>
   </div>
 </template>
 
 <script>
+import ButtonComponent from "./components/ButtonComponent";
+
 export default {
   name: "app",
-  data() {
-    return {
-      assignments: [
-        {
-          id: 1,
-          name: 'Task 1',
-          complete: false
-        },
-        {
-          id: 2,
-          name: 'Task 2',
-          complete: false
-        },
-        {
-          id: 3,
-          name: 'Task 3',
-          complete: false
-        },
-      ]
-    }
-  },
-  computed: {
-    completedAssignments() {
-      return this.assignments.filter(assignment => assignment.complete)
-    },
-    inProgressAssignments() {
-      return this.assignments.filter(assignment => !assignment.complete)
-    }
+  components: {
+    ButtonComponent,
   }
+
 }
 </script>
 
 <style scoped>
-.text-green {
-  color: green;
-}
 
-.text-red {
-  color: red;
-}
 </style>
---
+++ components/ButtonComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button :class="{
+      'button' : true,
+      'text-primary' : type === 'primary',
+      'text-secondary' : type === 'secondary'
+    }">
+      <slot/>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+  props: {
+    type: {
+      type: String,
+      default: "primary"
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  background: white;
+  padding: 5px 15px;
+  border-radius: 5px;
+}
+
+.text-primary {
+  color: blue;
+}
+
+.text-secondary {
+  color: green;
+}
+</style>
